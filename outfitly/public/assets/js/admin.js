879bc3db48b8a962266efb465d6fb633e56ec648
anin <101249091+adninin@users.noreply.github.com>
2025-12-21 01:00:32 +0700

    Delete outfitly directory

--- outfitly/public/assets/js/admin.js
+++
@@ -1,9 +0,0 @@
-// Bisa diisi interaksi kecil nantinya, contoh highlight menu
-const menuItems = document.querySelectorAll('.menu li');
-
-menuItems.forEach(item => {
-  item.addEventListener('click', () => {
-    document.querySelector('.menu li.active').classList.remove('active');
-    item.classList.add('active');
-  });
-});
